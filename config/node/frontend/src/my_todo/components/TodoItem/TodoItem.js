a396416a1ac022f455e70e42706b40ce901c77ce
zerom007 <zerom.kisiel@student.wat.edu.pl>
2025-03-19 11:24:37 +0100

    WIP: Add ToDo app

---
+++ config/node/frontend/src/my_todo/components/TodoItem/TodoItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+function TodoItem({ id, zadanie, listItemRemover }) {
+  console.log(id, zadanie);
+  return (
+    <div key={id}>
+      Nr {id}. Opis zadania: {zadanie}
+      <button onClick={(e) => listItemRemover(id)}>REMOVE</button>
+    </div>
+  );
+}
+export default TodoItem;
