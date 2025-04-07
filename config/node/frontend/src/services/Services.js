e56bec8ed960076d1e91cb754161f3c9770d3709
zerom007 <zerom.kisiel@student.wat.edu.pl>
2025-04-07 14:56:27 +0200

    wip: services buttons update

--- config/node/frontend/src/services/Services.js
+++ config/node/frontend/src/services/Services.js
@@ -13,6 +13,14 @@ function Services() {
           <div className="services_top">
             <h1 className="home_title">Dostępne usługi</h1>
           </div>
+          <div className="services_buttons">
+            <button className="services_button" id="mapa">
+              MAPA
+            </button>
+            <button className="services_button" id="lista">
+              WYKAZ lub lista CZEGOŚ
+            </button>
+          </div>
         </div>
         <div className="services_bottomRight">
           <div className="services_imageItem">
